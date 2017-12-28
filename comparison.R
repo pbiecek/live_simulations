b6e92cb2cdc6a4c07c8d9be52c3dbf7d20ae51c1
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2017-12-28 22:06:57 +0100

    different packages/methods comparison: TCGA data - init

---
+++ comparison.R
@@ -0,0 +1,56 @@
+### Libraries. ----
+# install.packages("lime")
+# install.packages("randomForestExplainer")
+# install.packages("xgboostExplainer")
+library(lime)
+library(mlr)
+library(live)
+library(tidyverse)
+library(MLExpRessoData)
+library(randomForest)
+### Code from TCGA vignette. ----
+set.seed(1)
+# homemade functions
+predfun <- function(model, newdata, ...) {
+  randomForest:::predict.randomForest(model, newdata = newdata, type = "prob")[,1]
+}
+# very basic normalisation
+BRCA <- MLExpRessoData::BRCA_mRNAseq_all_surv
+BRCA[,-(1:2)] <- 1000000*BRCA[,-(1:2)]/rowSums(BRCA[,-(1:2)])
+options(expressions = 500000)
+colnames(BRCA) <- make.names(colnames(BRCA))
+# try first 10000 variables
+nData <- BRCA[,-1]
+# pvals <- sapply(3:ncol(BRCA), function(i) wilcox.test(BRCA[,i]~BRCA[,2])$p.value) # takes a short while
+save(pvals, file = "pvals.rda")
+load("pvals.rda")
+# Training the black box.
+nData <- BRCA[,c(2,2 + which(pvals < 0.001))]
+trees <- randomForest(survival_status~., data = nData, ntree=10000)
+nind1 <- sample(which(nData$survival_status == 1), 100, replace = TRUE)
+nind2 <- sample(which(nData$survival_status == 0), 100, replace = TRUE)
+nData <- nData[c(nind1, nind2),]
+### Explaining the model. ----
+# 1. Using randomForestExplainer - globally.
+# 2. Using lime package - locally.
+# 3. Using live package - locally.
+similar <- sample_locally(data = nData,
+                          explained_instance = nData[2,],
+                          explained_var = "survival_status",
+                          size = 2000)
+similar <- add_predictions(nData, similar,
+                           black_box_model = trees,
+                           predict_fun = predfun)
+trained <- fit_explanation(live_object = similar,
+                           white_box = "regr.lm",
+                           selection = TRUE)
+plot_explanation(trained,
+                 regr_plot_type = "forestplot",
+                 explained_instance = nData[1,])
+plot_explanation(trained,
+                 regr_plot_type = "waterfallplot",
+                 explained_instance = nData[1,])
+### Visualization tools.
+# 1. PDP-plots.
+# 2. Plots from lime package.
+# 3. Plots from live.
