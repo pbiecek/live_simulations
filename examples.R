ee7f6ba3faddfe39876d6f7428f37d84ea96b13c
Mateusz Staniak <mtst@mstaniak.pl>
2018-03-20 12:35:13 +0100

    baseline = intercept

--- examples.R
+++ examples.R
@@ -12,8 +12,7 @@ wine_svm <- svm(quality ~., data = winequality_red)
 
 wine_expl <- lime(winequality_red[, -nc], wine_svm)
 model_type.svm <- function(x, ...) "regression"
-svm_explained <- lime::explain(nobs,
-                               wine_expl, n_features = 10)
+svm_explained <- lime::explain(nobs, wine_expl, n_features = 10)
 
 plot_features(svm_explained)
 plot_explanations(svm_explained)
@@ -21,5 +20,6 @@ plot_explanations(svm_explained)
 # breakDown
 explain_bd <- broken(wine_svm, new_observation = nobs,
                      data = winequality_red[, -nc],
-                     predict.function = kernlab:::predict)
+                     predict.function = kernlab:::predict,
+                     baseline = "Intercept")
 plot(explain_bd)
