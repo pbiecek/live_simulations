601b70841a88025988b48c886320130962fa7ea0
Mateusz Staniak <mtst@mstaniak.pl>
2018-03-20 12:08:42 +0100

    cleaned, new obs

--- examples.R
+++ examples.R
@@ -4,30 +4,22 @@ library(corrplot)
 library(lime)
 library(breakDown)
 
-corrplot(cor(winequality_red))
-
 load("winequality_red.rda")
+nc <- which(colnames(winequality_red) == "quality")
+nobs <- winequality_red[5, -nc]
+
 wine_svm <- svm(quality ~., data = winequality_red)
 
-nc <- which(colnames(winequality_red) == "quality")
 wine_expl <- lime(winequality_red[, -nc], wine_svm)
 model_type.svm <- function(x, ...) "regression"
-svm_explained <- lime::explain(winequality_red[88, -nc],
+svm_explained <- lime::explain(nobs,
                                wine_expl, n_features = 10)
 
 plot_features(svm_explained)
 plot_explanations(svm_explained)
 
 # breakDown
-explain_bd <- broken(wine_svm, new_observation = winequality_red[88, -nc],
+explain_bd <- broken(wine_svm, new_observation = nobs,
                      data = winequality_red[, -nc],
                      predict.function = kernlab:::predict)
 plot(explain_bd)
-
-### Maybe write an issue ----
-# data("WhiteWine")
-# white_svm <- svm(quality ~., data = WhiteWine)
-# white_ice <- ice(white_svm, WhiteWine, WhiteWine$quality,
-#                  predictor = "pH")
-# plot(wine_ice, plot_points_indices = 1, frac_to_plot = 1,
-#      plot_pdp = F)
