84c8904185dfc0c73a7e788313671d282531e91a
Mateusz Staniak <mtst@mstaniak.pl>
2018-03-28 21:55:17 +0200

    winequality_red -> wine

--- examples.R
+++ examples.R
@@ -1,7 +1,6 @@
 # Examples: explaining single predictions.
 # Libraries & data ----
 library(e1071)
-library(corrplot)
 library(lime)
 library(breakDown)
 library(live)
@@ -10,13 +9,13 @@ library(mlr)
 library(tidyr)
 library(dplyr)
 library(xtable)
-load("winequality_red.rda")
-nc <- which(colnames(winequality_red) == "quality")
-nobs <- winequality_red[5, -nc]
+load("rda_files/wine.rda")
+nc <- which(colnames(wine) == "quality")
+nobs <- wine[5, -nc]
 # Model ----
-wine_svm <- svm(quality ~., data = winequality_red)
+wine_svm <- svm(quality ~., data = wine)
 # LIME ----
-wine_expl <- lime(winequality_red, wine_svm)
+wine_expl <- lime(wine, wine_svm)
 model_type.svm <- function(x, ...) "regression"
 svm_explained <- lime::explain(nobs, wine_expl, n_features = 11)
 # LIME: plot
@@ -30,16 +29,16 @@ explain_bd <- broken(wine_svm, new_observation = nobs,
 # breakDown: plot
 plot(explain_bd)
 # live
-wine_sim <- sample_locally(winequality_red, winequality_red[5, ], "quality", 500)
-wine_sim_svm <- add_predictions(winequality_red, wine_sim, wine_svm)
+wine_sim <- sample_locally(wine, winequality_red[5, ], "quality", 500)
+wine_sim_svm <- add_predictions(wine, wine_sim, wine_svm)
 wine_expl_live <- fit_explanation(wine_sim_svm, "regr.lm")
 wine_expl_live2 <- fit_explanation(wine_sim_svm, "regr.lm", selection = T)
-plot_explanation(wine_expl_live, "waterfallplot", winequality_red[5, ])
-plot_explanation(wine_expl_live, "forestplot", winequality_red[5, ])
-plot_explanation(wine_expl_live2, "waterfallplot", winequality_red[5, ])
+plot_explanation(wine_expl_live, "waterfallplot", wine[5, ])
+plot_explanation(wine_expl_live, "forestplot", wine[5, ])
+plot_explanation(wine_expl_live2, "waterfallplot", wine[5, ])
 # SHAP
 # requires the use of mlr
-tsk <- makeRegrTask("wine", winequality_red, "quality")
+tsk <- makeRegrTask("wine", wine, "quality")
 shp <- shapley(5, model = train("regr.svm", tsk), task = tsk)
 # plot.shapley.singleValue(1, model = train("regr.lm", tsk), task = tsk)
 brk <- data.frame(var = explain_bd$variable_name, explain_bd$contribution)
